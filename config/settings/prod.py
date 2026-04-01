1c37cbcfd4a1d4adcbc0bc3c7d93b568b8662443
oluwatosinjegede <jegedeoluwatosin10@gmail.com>
2026-04-01 13:23:27 +0100

    Fix Infinite HTTPS Redirect Loop

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,9 +4,11 @@ import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.railway.app']  # safer wildcard
+ALLOWED_HOSTS = ['.railway.app']
 
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
